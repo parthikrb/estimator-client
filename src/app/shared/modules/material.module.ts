495ffac6a5c797ea296f4598e3975a794d692c16
parthikrb <parthi.krb@gmail.com>
2020-03-25 13:47:39 +0530

    Updated the header template

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -12,6 +12,7 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatSliderModule} from '@angular/material/slider';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import {NgModule} from '@angular/core';
 
@@ -30,7 +31,8 @@ import {NgModule} from '@angular/core';
     MatProgressSpinnerModule,
     MatTooltipModule,
     MatListModule,
-    MatDialogModule
+    MatDialogModule,
+    MatToolbarModule
   ],
   exports: [
     MatButtonModule,
@@ -46,7 +48,8 @@ import {NgModule} from '@angular/core';
     MatProgressSpinnerModule,
     MatTooltipModule,
     MatListModule,
-    MatDialogModule
+    MatDialogModule,
+    MatToolbarModule
   ],
 })
 
